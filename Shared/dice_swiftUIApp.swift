781e07e0dcee61bd87aae1e50b57f078182429eb
G3N <yap@corekara.co.jp>
2022-02-05 17:20:53 +0900

    Initial Commit

---
+++ Shared/dice_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_swiftUIApp.swift
+//  Shared
+//
+//  Created by Yap Feng Yuan on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct dice_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
